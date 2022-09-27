7fef49cbc17aa0e9176044b69a9c8152454ef338
Esteban <nameless.voidd@gmail.com>
2022-09-27 14:20:02 -0300

    cambio cors

--- Logica BackEnd/portfolio/src/main/java/com/estebancandioti/portfolio/controller/Controller.java
+++ Logica BackEnd/portfolio/src/main/java/com/estebancandioti/portfolio/controller/Controller.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="*")
 public class Controller {
         //Injeccion de los servicios
     @Autowired
--- Logica BackEnd/portfolio/src/main/java/com/estebancandioti/portfolio/security/AuthApi.java
+++ Logica BackEnd/portfolio/src/main/java/com/estebancandioti/portfolio/security/AuthApi.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="*")
 public class AuthApi {
     @Autowired
     AuthenticationManager authManager;
