3e3693bcbfbb7fa868dd83dd820225f693fe5cdc
EstebanCandioti <101424840+EstebanCandioti@users.noreply.github.com>
2022-10-01 21:42:41 -0300

    deploy jwt

--- Logica BackEnd/portfolio/src/main/java/com/estebancandioti/portfolio/AplicationSecurityConfig.java
+++ Logica BackEnd/portfolio/src/main/java/com/estebancandioti/portfolio/AplicationSecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+//hola 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled= true)
